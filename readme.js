f5d27565cb5d2df73fa1b23d07df5224b7c65804
Jérémy Cheynet <jeremy.cheynet@actronika.com>
2018-05-23 16:01:01 +0200

    doc: add documentation (readme)
    Signed-off-by: Jérémy Cheynet <jeremy.cheynet@actronika.com>

---
+++ readme.js
@@ -0,0 +1,49 @@
+const readmeContains = `
+# HTML force sensor viewer
+
+## Connection
+
+Connect a force sensor (*CZL616*) on input 0, 1, 2 or 3 :
+* Black wire on **g**
+* White wire on **\-**
+* Green wire on **\+**
+* Red wire on **5V**
+
+![](img/connection.jpg)
+
+## Install phidget server
+
+Go on Phidget website and install drivers :
+* [Windows](https://www.phidgets.com/docs/OS_-_Windows#Quick_Downloads)
+* [GNU/Linux](https://www.phidgets.com/docs/OS_-_Linux#Quick_Downloads)
+* [MacOS](https://www.phidgets.com/docs/OS_-_OS_X#Quick_Downloads)
+
+## Launch phidget server
+
+### Windows
+
+Launch Phidget application and click on *Network Server* tab.
+
+![](img/Windows_networkserver.PNG)
+
+Select \`Automatic\` in *startup type*
+Then click on \`Start\`
+
+### GNU/Linux and MacOS
+
+Not yet tested.
+
+## Launch application
+
+### Online version
+
+* Open your favorite browser
+* Go to [https://actronikasas.github.io/phidget-html-viewer/](https://actronikasas.github.io/phidget-html-viewer/)
+
+### Local version
+
+* Go to [https://github.com/ActronikaSAS/phidget-html-viewer](https://github.com/ActronikaSAS/phidget-html-viewer).
+* Click on \`Clone or download\` and download *zip* file.
+* Open the file \`phidget-html-viewer-master/index.html\` in your favorite browser.
+
+`;
